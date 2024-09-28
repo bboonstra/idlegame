2f11a775df5ed82b2867aa87e3826836cb144312
Ben Boonstra <bboonstra26@gmail.com>
2024-09-27 22:50:14 -0500

    Change to main.py

--- idlegame/game.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Welcome to Idle Game!")
-    # Game logic here
-
-if __name__ == "__main__":
-    main()
---
+++ idlegame/main.py
@@ -0,0 +1,7 @@
+# main.py
+def main():
+    print("Welcome to the Idle Game!")
+    # Your game logic goes here
+
+if __name__ == '__main__':
+    main()
