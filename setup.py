2841a10ab7ce1a0062ca0a7e74809e1a025cfd11
Ben Boonstra <bboonstra26@gmail.com>
2024-09-28 23:46:33 -0500

    1.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='idlegame',
-    version='1.5.2',
+    version='1.6.0',
     packages=find_packages(),
     install_requires=[
         'colorama',
